9909fc99eaeeb299ad97be507c26bd0c97679765
Hanno Hecker <vetinari@ankh-morp.org>
2008-04-19 07:25:06 +0000

    - bump to v0.4
    git-svn-id: https://svn.ankh-morp.org:8081/tools/paste-dn@11 c838b21e-a1c1-499c-a498-79f35c24d3eb

--- paste-dn.pl
+++ paste-dn.pl
@@ -40,7 +40,7 @@ binmode(STDOUT, ":utf8");
 binmode(STDERR, ":utf8");
 
 $0 =~ s#.*/##;
-my $VERSION = '0.3 ($Rev$)';
+my $VERSION = '0.4 ($Rev$)';
 my $settings = $ENV{HOME}."/.paste-dn.rc";
 
 ## Don't change, edit $settings file:
